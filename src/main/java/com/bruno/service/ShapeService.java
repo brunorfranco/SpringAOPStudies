193b822fa076d65a1b010f737ef00c11a90ff479
Bruno Rodrigues Franco <bruno.rodrigues.franco1@gmail.com>
2015-07-26 15:25:18 +0100

    Initial Setup
    Project to study Spring AOP created, with all necessary dependencies and
    first simple logging aop behaviour.

---
+++ src/main/java/com/bruno/service/ShapeService.java
@@ -0,0 +1,27 @@
+package com.bruno.service;
+
+import com.bruno.model.Circle;
+import com.bruno.model.Triangle;
+
+public class ShapeService {
+	
+	private Circle circle;
+	private Triangle triangle;
+	
+	public Circle getCircle() {
+		return circle;
+	}
+	
+	public void setCircle(Circle circle) {
+		this.circle = circle;
+	}
+	
+	public Triangle getTriangle() {
+		return triangle;
+	}
+	
+	public void setTriangle(Triangle triangle) {
+		this.triangle = triangle;
+	}
+
+}
